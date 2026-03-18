b81ad4aa9950cf5c0a23724606b4f0e81d397a37
Serializer <218253225+Web3-Serializer@users.noreply.github.com>
2026-03-18 15:03:11 +0100

    Update chal-deobf.js

--- reverses/chal-deobf.js
+++ reverses/chal-deobf.js
@@ -1,5 +1,7 @@
+// offset is dynamic
 const OFFSET = 24 + 45 + (44 + 87); // = 200
 
+// only decode, angular dead code out lmao
 function decode(message) {
   return message.replace(/./g, function(char, position) {
     return String.fromCharCode(
@@ -8,4 +10,5 @@ function decode(message) {
   });
 }
 
+// what a challenge ! just need to xor that with session and ur done
 console.log(decode('SywSqpJUmN6K2y8Jy2LQbZMfDbotaXmzzUHmKRdthivrzHvpwgEvuHWegRVB5CZ6O5Nl13EaWxYjMGf8Wc0yUvauqyqTr18ZDMTI'));
